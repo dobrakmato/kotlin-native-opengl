f1a85a4217a04d3a06e7a7b77992817431c08fbc
Matej <dobrakmato@gmail.com>
2018-12-17 13:20:24 +0100

    remove unneeded conversions

--- src/bfMain/kotlin/bf/image.kt
+++ src/bfMain/kotlin/bf/image.kt
@@ -4,30 +4,30 @@ import io.ByteBuffer
 import kotlin.math.max
 
 /* image flags */
-const val BF_IMAGE_FLAG_LZ4 = 1
-const val BF_IMAGE_FLAG_LZ4_HC = 2
-const val BF_IMAGE_FLAG_DXT = 4
-const val BF_IMAGE_FLAG_FLOAT = 8
-const val BF_IMAGE_FLAG_16_BIT = 16
-const val BF_IMAGE_FLAG_VERTICAL_FLIP = 32
-const val BF_IMAGE_FLAG_GAMMA_SRGB = 64
-const val BF_IMAGE_FLAG_SKYBOX = 128
+const val BF_IMAGE_FLAG_LZ4: UByte = 1u
+const val BF_IMAGE_FLAG_LZ4_HC: UByte = 2u
+const val BF_IMAGE_FLAG_DXT: UByte = 4u
+const val BF_IMAGE_FLAG_FLOAT: UByte = 8u
+const val BF_IMAGE_FLAG_16_BIT: UByte = 16u
+const val BF_IMAGE_FLAG_VERTICAL_FLIP: UByte = 32u
+const val BF_IMAGE_FLAG_GAMMA_SRGB: UByte = 64u
+const val BF_IMAGE_FLAG_SKYBOX: UByte = 128u
 
 inline class BfImageFlags(val value: UByte) {
-    inline fun lz4() = (value.toInt() and BF_IMAGE_FLAG_LZ4) == BF_IMAGE_FLAG_LZ4
-    inline fun lz4hc() = (value.toInt() and BF_IMAGE_FLAG_LZ4_HC) == BF_IMAGE_FLAG_LZ4_HC
-    inline fun dxt() = (value.toInt() and BF_IMAGE_FLAG_DXT) == BF_IMAGE_FLAG_DXT
-    inline fun float() = (value.toInt() and BF_IMAGE_FLAG_FLOAT) == BF_IMAGE_FLAG_FLOAT
-    inline fun is16bit() = (value.toInt() and BF_IMAGE_FLAG_16_BIT) == BF_IMAGE_FLAG_16_BIT
-    inline fun verticallyFlipped() = (value.toInt() and BF_IMAGE_FLAG_VERTICAL_FLIP) == BF_IMAGE_FLAG_VERTICAL_FLIP
-    inline fun srgb() = (value.toInt() and BF_IMAGE_FLAG_GAMMA_SRGB) == BF_IMAGE_FLAG_GAMMA_SRGB
-    inline fun skybox() = (value.toInt() and BF_IMAGE_FLAG_SKYBOX) == BF_IMAGE_FLAG_SKYBOX
+    inline fun lz4() = (value and BF_IMAGE_FLAG_LZ4) == BF_IMAGE_FLAG_LZ4
+    inline fun lz4hc() = (value and BF_IMAGE_FLAG_LZ4_HC) == BF_IMAGE_FLAG_LZ4_HC
+    inline fun dxt() = (value and BF_IMAGE_FLAG_DXT) == BF_IMAGE_FLAG_DXT
+    inline fun float() = (value and BF_IMAGE_FLAG_FLOAT) == BF_IMAGE_FLAG_FLOAT
+    inline fun is16bit() = (value and BF_IMAGE_FLAG_16_BIT) == BF_IMAGE_FLAG_16_BIT
+    inline fun verticallyFlipped() = (value and BF_IMAGE_FLAG_VERTICAL_FLIP) == BF_IMAGE_FLAG_VERTICAL_FLIP
+    inline fun srgb() = (value and BF_IMAGE_FLAG_GAMMA_SRGB) == BF_IMAGE_FLAG_GAMMA_SRGB
+    inline fun skybox() = (value and BF_IMAGE_FLAG_SKYBOX) == BF_IMAGE_FLAG_SKYBOX
 }
 
-inline class BfImageExtra(val value: Byte) {
-    fun hasMipmaps() = (value.toInt() and 8) == 8
-    fun includedMipmaps() = max(1, (value.toInt() and 0b11110000) shr 4)
-    fun numberOfChannels() = (value.toInt() and 0b00000111)
+inline class BfImageExtra(val value: UByte) {
+    inline fun hasMipmaps() = (value and 8u) == 8u.toUByte()
+    inline fun includedMipmaps() = max(1, (value.toInt() and 0b11110000) shr 4)
+    inline fun numberOfChannels() = (value.toInt() and 0b00000111)
 }
 
 /* extend ByteBuffer to allow storing image data structures */
